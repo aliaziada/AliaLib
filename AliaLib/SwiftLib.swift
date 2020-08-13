43f65b4aa03e6a3dde5768cabc88f557f12e412a
alia ziada <alia92ziada@gmail.com>
2020-08-13 14:01:30 +0200

    Added test cases

---
+++ AliaLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  AliaLib
+//
+//  Created by Alia Ziada on 8/13/20.
+//  Copyright © 2020 Alia Ziada. All rights reserved.
+//
+
+import Foundation
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Alia Ziada on 8/13/20.
+//  Copyright © 2020 Alia Ziada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
