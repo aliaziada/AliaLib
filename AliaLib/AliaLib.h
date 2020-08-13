bb84da40db7aacdce03a914d5c7dc02987a244aa
alia ziada <alia92ziada@gmail.com>
2020-08-13 13:09:27 +0200

    Initial Commit

---
+++ AliaLib/AliaLib.h
@@ -0,0 +1,19 @@
+//
+//  AliaLib.h
+//  AliaLib
+//
+//  Created by Alia Ziada on 8/13/20.
+//  Copyright © 2020 Alia Ziada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AliaLib.
+FOUNDATION_EXPORT double AliaLibVersionNumber;
+
+//! Project version string for AliaLib.
+FOUNDATION_EXPORT const unsigned char AliaLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AliaLib/PublicHeader.h>
+
+
